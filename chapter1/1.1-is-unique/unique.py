987c252fbacab98964dad38fc3634454f82fe8c7
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-11-20 18:59:58 +0200

    Adds sample exercies for the first chapter

---
+++ chapter1/1.1-is-unique/unique.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import json
+
+def read_json(file_path):
+	with open(file_path, 'r') as file:
+		data = json.load(file)
+	return data
+
+def check_unique(string):
+	if len(string) <= 1:
+		return True
+	sorted_str = sorted(string)
+	next_char = sorted_str[1]
+	for char in sorted_str:
+		if char == next_char:
+			return False
+		next_char = char
+	return True
+
+data = read_json("sample.json")
+for i in data.values():
+	print(check_unique(i))
+
